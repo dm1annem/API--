610f0ce63f264c8e3a99201480b9994cf9242bad
Нуфтанчики <300225@mail.ru>
2021-11-14 20:23:28 +0300

    добавил юзеров в апи отзывов

--- api/api/urls.py
+++ api/api/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+import reviews
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    
+    path('reviews/', include('reviews.urls')),
 ]
