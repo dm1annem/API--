23c9c3a5260e3416f580d2733a81ad9dc51d74bf
Нуфтанчики <300225@mail.ru>
2021-11-14 18:56:26 +0300

    создал модель Оезывы Reviews

--- api/reviews/models.py
+++ api/reviews/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
-class Reviews(models.Model):
-    title = models.CharField(max_length=120, blank=True, verbose_name='Заголовок')
-    video_link = models.URLField(max_length=200, null=True, blank=True, verbose_name='Ссылка на видео')
-    img = models.ImageField(upload_to='reviews', null=True, blank=True, verbose_name='Фото')
-    text = models.TextField(null=True, blank=True, verbose_name='Текст отзыва')
+class Review(models.Model):
+    title = models.CharField(max_length=120, blank=True, verbose_name='Заголовок', default='')
+    video_link = models.URLField(max_length=200, null=True, blank=True, verbose_name='Ссылка на видео', default='')
+    img = models.ImageField(upload_to='reviews', null=True, blank=True, verbose_name='Фото', default='')
+    text = models.TextField(null=True, blank=True, verbose_name='Текст отзыва', default='')
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубликовано')
-    comment = models.CharField(max_length=220, blank=True, verbose_name='Комментарий')
+    comment = models.CharField(max_length=220, blank=True, verbose_name='Комментарий', default='')
+    owner = models.ForeignKey('auth.User', related_name='rewiews', on_delete=models.CASCADE)
 
     class Meta:
         verbose_name_plural = 'Отзывы'
